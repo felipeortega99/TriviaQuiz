843eb4a2313b7ed57948e1a18ffc7e4c6b9540ee
Felipe Ortega Casillas <felipeo@advancio.org>
2019-09-03 11:28:05 -0700

    Create home layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  {
+    path: "home",
+    loadChildren: () =>
+      import("./pages/home/home.module").then(m => m.HomePageModule)
+  },  { path: 'game', loadChildren: './pages/game/game.module#GamePageModule' }
+
 ];
 
 @NgModule({
@@ -12,4 +17,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
